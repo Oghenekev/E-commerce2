0aa2887a9314bf4a46f929bda3450ed8b7e65f9e
Oghenekev <omowareoghenekevwe384@gmail.com>
2025-07-09 00:59:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("menu")
+
+let menuBox = document.querySelector(".menu-box")
+
+menu.addEventListener('click' , function(){
+    menuBox.classList.toggle("slider");
+})
